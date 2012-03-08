87e1a487b56e286f47da52b5dd596d121f2c8332
xiaomo <xiaomo@Thx.(none)>
2012-03-08 15:36:48 +0800

    Net::URL::Frontier

--- Net/URL/Frontier.pm
+++ Net/URL/Frontier.pm
@@ -1,4 +1,4 @@
-package Net::URL::Formatter;
+package Net::URL::Frontier;
 
 use warnings;
 use strict 'vars';
@@ -9,11 +9,39 @@ use vars qw/$VERSION @ISA @EXPORT @EXPORT_OK/;
 
 $VERSION   = "1.0";
 @ISA       = qw/Exporter/;
-@EXPORT    = qw/beauty get_protocol get_host get_location/;
+@EXPORT    = qw/beauty digest/;
 @EXPORT_OK = @EXPORT;
 
+BEGIN { 
+    require URI;
+    require Digest::HMAC_SHA1;
+    require MIME::Base64;
+}
+
 #-------------------------------------
+sub type {
+    my $url = shift;
+    if ($url =~ /^http:\/\//) {
+        return "http://";
+    } elsif ($url =~ /^https:\/\//) {
+        return "https://";
+    } else {
+        croak "Unknown!";
+    }
+}
+
 sub beauty {
+    my $url = shift;
+    my $uri = URI->new($url);
+    return type($url)
+         . $uri->host_port
+         . $uri->path;
+}
+
+sub digest {
+    use Digest::HMAC_SHA1;
+    use MIME::Base64;
+    return encode_base64(Digest::HMAC_SHA1->new(shift)->digest);
 }
 
 #------------------------------------
